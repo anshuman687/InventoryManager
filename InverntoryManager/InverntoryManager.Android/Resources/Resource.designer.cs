ff83fa823f4a2d4692fe6f44850115c9a86c6d16
Anshuman Tiwari <anshuman@klizos.com>
2020-11-17 00:18:51 +0530

    Dashboard

--- InverntoryManager/InverntoryManager.Android/Resources/Resource.designer.cs
+++ InverntoryManager/InverntoryManager.Android/Resources/Resource.designer.cs
@@ -8400,22 +8400,25 @@ namespace InverntoryManager.Droid
 			public const int notify_panel_notification_icon_bg = 2131165307;
 			
 			// aapt resource value: 0x7F07007C
-			public const int splash = 2131165308;
+			public const int slide = 2131165308;
 			
 			// aapt resource value: 0x7F07007D
-			public const int splash_screen = 2131165309;
+			public const int splash = 2131165309;
 			
 			// aapt resource value: 0x7F07007E
-			public const int tooltip_frame_dark = 2131165310;
+			public const int splash_screen = 2131165310;
 			
 			// aapt resource value: 0x7F07007F
-			public const int tooltip_frame_light = 2131165311;
+			public const int tooltip_frame_dark = 2131165311;
 			
 			// aapt resource value: 0x7F070080
-			public const int trash = 2131165312;
+			public const int tooltip_frame_light = 2131165312;
 			
 			// aapt resource value: 0x7F070081
-			public const int update = 2131165313;
+			public const int trash = 2131165313;
+			
+			// aapt resource value: 0x7F070082
+			public const int update = 2131165314;
 			
 			static Drawable()
 			{
